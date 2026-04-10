be29897bacee42ea37b059d3088d8b4cc9600bcd
Frolunddd <oliver.frolund@gmail.com>
2026-04-10 10:55:55 +0200

    Update Classes and Objeckts.cs

--- CodeExercise/ConsoleApp1/Classes and Objeckts.cs
+++ CodeExercise/ConsoleApp1/Classes and Objeckts.cs
@@ -17,4 +17,23 @@ public class Test //Classes and Objects 1
 
 
 
+//	Classes And Objects 2
+public class Test2
+{
+    public Node getList()
+    {
+        Node n;
+        n = new Node();
+        n.TAL = 1;
+        n.NEXT = new Node();
+        n.NEXT.TAL = 2;
+        return n;
+    }
+    public class Node
+    {
+        public int TAL;
+        public Node NEXT;
+    }
+}
+
 
