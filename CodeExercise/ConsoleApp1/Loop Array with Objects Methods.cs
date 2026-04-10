b86b79ecaccab61c41d039e812e7f3ad0c3aea58
Frolunddd <oliver.frolund@gmail.com>
2026-04-10 11:26:32 +0200

    Create Loop Array with Objects Methods.cs

---
+++ CodeExercise/ConsoleApp1/Loop Array with Objects Methods.cs
@@ -0,0 +1,27 @@
+﻿//	Loop Array with Objects Methods 1
+
+public class Test1
+{
+
+    public Car ArrayWithObjects(Car[] carsArr)
+    {
+        int i = 0;
+        int carMedStørstKM = carsArr[0].KM;
+        var Car = carsArr[0];
+
+        while (i < carsArr.Length)
+        {
+            if (carsArr[i].KM > carMedStørstKM)
+            {
+                carMedStørstKM = carsArr[i].KM;
+                Car = carsArr[i];
+            }
+            i++;
+        }
+        return Car;
+    }
+}
+public class Car
+{
+    public int KM = 0;
+}
