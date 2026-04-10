7bf71d5a088ef33c2f9242df9d3ef7a7d3773f47
Frolunddd <oliver.frolund@gmail.com>
2026-04-10 10:40:49 +0200

    Add opgave classes and objects 1

---
+++ CodeExercise/ConsoleApp1/Classes and Objeckts.cs
@@ -0,0 +1,20 @@
+﻿// Classes and Objects 1
+
+public class Test //Classes and Objects 1
+{
+    public Node getNode()
+    {
+        Node node;
+        node = new Node();
+        node.TAL = 1;
+        return node;
+    }
+    public class Node
+    {
+        public int TAL;
+    }
+}
+
+
+
+
